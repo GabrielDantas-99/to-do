dcb92fa6d8db940d92410d0ca466cbb0dd72ecc5
GabrielDantas-99 <gabrieldantaslp@gmail.com>
2022-05-17 19:00:13 -0300

    Criação do projeto e todo class

---
+++ src/main/java/com/gabriel/todo/domain/Todo.java
@@ -0,0 +1,85 @@
+package com.gabriel.todo.domain;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Todo {
+
+	private Long Id;
+	private String titulo;
+	private String descricao;
+	private LocalDateTime deadLine;
+	private Boolean finalizada = false;
+
+	public Todo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Todo(Long id, String titulo, String descricao, LocalDateTime deadLine, Boolean finalizada) {
+		super();
+		Id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.deadLine = deadLine;
+		this.finalizada = finalizada;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDateTime getDeadLine() {
+		return deadLine;
+	}
+
+	public void setDeadLine(LocalDateTime deadLine) {
+		this.deadLine = deadLine;
+	}
+
+	public Boolean getFinalizada() {
+		return finalizada;
+	}
+
+	public void setFinalizada(Boolean finalizada) {
+		this.finalizada = finalizada;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		return Objects.equals(Id, other.Id);
+	}
+
+}
