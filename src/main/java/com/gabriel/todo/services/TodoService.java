337a532da5e70333bece94917aab3d98f0ed833e
GabrielDantas-99 <gabrieldantaslp@gmail.com>
2022-05-17 19:40:20 -0300

    Todo service e resource e findById

---
+++ src/main/java/com/gabriel/todo/services/TodoService.java
@@ -0,0 +1,24 @@
+package com.gabriel.todo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabriel.todo.domain.Todo;
+import com.gabriel.todo.repositories.TodoRepository;
+
+// Camada responsável pela lógica de negócio
+
+@Service
+public class TodoService {
+
+	@Autowired
+	private TodoRepository repository;
+
+	public Todo findById(Long id) {
+		Optional<Todo> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+
+}
